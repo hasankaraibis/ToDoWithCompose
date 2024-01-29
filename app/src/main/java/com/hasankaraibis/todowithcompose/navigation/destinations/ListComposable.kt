b722b4d8482d8f7c2e41e8629da7a7e5855b1a3d
hasanhusky <husky9832@gmail.com>
2024-01-30 00:08:24 +0300

    [feat] list screen created, fab created. [chore] kotlin compose versions(to 1.6.0), dependency versions(to 1.6.0), kotlinCompilerExtensionVersion(to 1.4.1) updated.

--- app/src/main/java/com/hasankaraibis/todowithcompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/hasankaraibis/todowithcompose/navigation/destinations/ListComposable.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.hasankaraibis.todowithcompose.ui.screens.list.ListScreen
 import com.hasankaraibis.todowithcompose.util.Constants.LIST_ARGUMENT_KEY
 import com.hasankaraibis.todowithcompose.util.Constants.LIST_SCREEN
 
@@ -16,6 +17,6 @@ fun NavGraphBuilder.listComposable(
             type = NavType.StringType
         })
     ) {
-
+        ListScreen(navigateToTaskScreen = navigateToTaskScreen)
     }
 }
