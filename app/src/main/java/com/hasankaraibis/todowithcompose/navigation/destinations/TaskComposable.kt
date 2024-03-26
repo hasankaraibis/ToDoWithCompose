6f3e2e7c135839dc3c17a02f91af4336af2798d5
hasanhusky <husky9832@gmail.com>
2024-03-26 22:55:30 +0300

    [feat] NewTaskAppBar and TaskScreen created.

--- app/src/main/java/com/hasankaraibis/todowithcompose/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/hasankaraibis/todowithcompose/navigation/destinations/TaskComposable.kt
@@ -1,21 +1,25 @@
 package com.hasankaraibis.todowithcompose.navigation.destinations
 
+import android.util.Log
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.hasankaraibis.todowithcompose.util.Action
 import com.hasankaraibis.todowithcompose.util.Constants
+import com.hasankaraibis.todowithcompose.util.Constants.TASK_ARGUMENT_KEY
+import com.hasankaraibis.todowithcompose.util.Constants.TASK_SCREEN
 
 fun NavGraphBuilder.taskComposable(
     navigateToListScreen: (Action) -> Unit
 ) {
     composable(
-        route = Constants.TASK_SCREEN,
-        arguments = listOf(navArgument(Constants.TASK_ARGUMENT_KEY) {
+        route = TASK_SCREEN,
+        arguments = listOf(navArgument(TASK_ARGUMENT_KEY) {
             type = NavType.IntType
         })
-    ) {
-
+    ) { navBackStachEntry ->
+        val taskId = navBackStachEntry.arguments!!.getInt(TASK_ARGUMENT_KEY)
+        Log.d("TaskComposable", taskId.toString())
     }
 }
