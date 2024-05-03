4dc1404bc60c9e4e26d0f93e0eaaf75c874f5fd2
hasanhusky <husky9832@gmail.com>
2024-05-03 23:41:45 +0300

    [feat] TaskContent page created.

--- app/src/main/java/com/hasankaraibis/todowithcompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/hasankaraibis/todowithcompose/ui/screens/task/TaskScreen.kt
@@ -1,10 +1,13 @@
 package com.hasankaraibis.todowithcompose.ui.screens.task
 
+import android.annotation.SuppressLint
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import com.hasankaraibis.todowithcompose.data.models.Priority
 import com.hasankaraibis.todowithcompose.data.models.ToDoTask
 import com.hasankaraibis.todowithcompose.util.Action
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun TaskScreen (
     selectedTask: ToDoTask?,
@@ -17,6 +20,15 @@ fun TaskScreen (
                      navigateToListScreen = navigateToListScreen
                  )
         },
-        content = {}
+        content = {
+            TaskContent(
+                title = "",
+                onTitleChange = {},
+                description = "",
+                onDescriptionChange = {},
+                priority = Priority.HIGH,
+                onPrioritySelected = {}
+            )
+        }
     )
 }
